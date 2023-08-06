9a5d876323a3c98bfb7acd796d23551485d88c6c
Pengpa0012 <peng.ilikha@gmail.com>
2023-08-06 18:38:56 +0800

    added player despawn

--- script/player.gd
+++ script/player.gd
@@ -5,6 +5,7 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -500.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var animatedSprite = $AnimatedSprite2D
+@onready var display_size = get_viewport().get_visible_rect().size
 
 func _physics_process(delta):
 	if not is_on_floor():
@@ -18,14 +19,21 @@ func _physics_process(delta):
 		velocity.y = JUMP_VELOCITY
 
 	var direction = Input.get_axis("ui_left", "ui_right")
+	
 	if direction:
 		if is_on_floor():
 			animatedSprite.play("Run")		
 		velocity.x = direction * SPEED
+		
 		if velocity.x == -300:
 			animatedSprite.flip_h = true
 		else:
-			animatedSprite.flip_h = false			
+			animatedSprite.flip_h = false		
+			
+		if self.position.x >= display_size.x:
+			self.position.x = 0
+		if self.position.x <= -10:
+			self.position.x = display_size.x
 	else:
 		if is_on_floor():
 			animatedSprite.play("Idle")		
