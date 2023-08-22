59f619f790224ee2334936c82f61e0f7df78b90c
Pengpa0012 <peng.ilikha@gmail.com>
2023-08-22 21:17:35 +0800

    added coin pickup sfx

--- script/rune.gd
+++ script/rune.gd
@@ -1,5 +1,6 @@
 extends Node2D
 @onready var GLOBAL = get_node("/root/Global")
+@onready var coinAudio = get_node("/root/world/SFX/coin")
 
 func _process(delta):
 	$AnimatedSprite2D.play("coin")
@@ -7,5 +8,6 @@ func _process(delta):
 
 func _on_area_2d_body_entered(body):
 	if "Player" in body.name:
+		coinAudio.play()
 		GLOBAL.SCORE += 2
 		self.queue_free()
